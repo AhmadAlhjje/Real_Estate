5d55c1be1b885bc0ecb4e661a8d777746dd62479
obada63 <ahmadobadaal7jje@gmail.com>
2025-03-20 03:03:32 +0300

    Add Dashboard  and PropertyList Files

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,30 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./NavBar.css";
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <nav className="navbar">
-      <div className="navbar-container">
-        <Link to="/" className="navbar-logo">
-          <h1>عقارات</h1>
-        </Link>
-        <ul className="navbar-menu">
-          <li>
-            <Link to="/contact" className="navbar-link">اتضل بنا</Link>
-          </li>
-          <li>
-            <Link to="/about" className="navbar-link">عن الموقع</Link>
-          </li>
-          <li>
-            <Link to="/realEstate" className="navbar-link">العقارات </Link>
-          </li>
-          <li>
-            <Link to="/" className="navbar-link">الرئيسية </Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <>
+      <nav className={`navbar ${isOpen ? "open" : ""}`}>
+        <div className="navbar-container">
+          {/* الحاوية التي تجمع زر القائمة مع الشعار */}
+          <div className="navbar-header">
+            <button className="menu-toggle" onClick={() => setIsOpen(!isOpen)}>
+              ☰
+            </button>
+            <Link to="/" className="navbar-logo">
+              <h1>عقارات</h1>
+            </Link>
+          </div>
+
+          {/* قائمة التنقل */}
+          <ul className={`navbar-menu ${isOpen ? "show" : ""}`}>
+            <li>
+              <Link to="/contact" className="navbar-link" onClick={() => setIsOpen(false)}>اتصل بنا</Link>
+            </li>
+            <li>
+              <Link to="/about" className="navbar-link" onClick={() => setIsOpen(false)}>عن الموقع</Link>
+            </li>
+            <li>
+              <Link to="/realEstate" className="navbar-link" onClick={() => setIsOpen(false)}>العقارات</Link>
+            </li>
+            <li>
+              <Link to="/" className="navbar-link" onClick={() => setIsOpen(false)}>الرئيسية</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      {/* لتعديل المسافة بين الشريط والمحتوى عند فتح القائمة */}
+      <div className={`content-offset ${isOpen ? "expanded" : ""}`}></div>
+    </>
   );
 };
 
