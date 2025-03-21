18aaa39ef7d43b1c426239e14d8128a91b69f6f0
obada63 <ahmadobadaal7jje@gmail.com>
2025-03-21 14:52:31 +0300

    Edit NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { FaUserCircle } from "react-icons/fa";
 import "./NavBar.css";
 
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [userMenuOpen, setUserMenuOpen] = useState(false);
 
   return (
     <>
@@ -28,12 +30,28 @@ const NavBar = () => {
               <Link to="/realEstate" className="navbar-link" onClick={() => setIsOpen(false)}>العقارات</Link>
             </li>
             <li>
-              <Link to="/AddProperty" className="navbar-link" onClick={() => setIsOpen(false)}> إضافة عقار</Link>
+              <Link to="/AddProperty" className="navbar-link" onClick={() => setIsOpen(false)}>إضافة عقار</Link>
             </li>
             <li>
               <Link to="/contact" className="navbar-link" onClick={() => setIsOpen(false)}>اتصل بنا</Link>
             </li>
           </ul>
+
+          {/* أيقونة المستخدم */}
+          <div className="user-menu">
+            <FaUserCircle 
+              size={30} 
+              className="user-icon" 
+              onClick={() => setUserMenuOpen(!userMenuOpen)} 
+            />
+            {userMenuOpen && (
+              <div className="user-dropdown">
+                <Link to="/login" className="user-dropdown-item">تسجيل الدخول</Link>
+                <Link to="/signup" className="user-dropdown-item">إنشاء حساب</Link>
+                <Link to="/favorites" className="user-dropdown-item">المفضلات</Link>
+              </div>
+            )}
+          </div>
         </div>
       </nav>
 
