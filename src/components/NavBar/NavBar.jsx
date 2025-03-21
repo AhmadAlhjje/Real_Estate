f592e07206d0f10f9125f87c9f9fdcfbeda237f4
obada63 <ahmadobadaal7jje@gmail.com>
2025-03-22 02:29:17 +0300

    Edit Navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,62 +1,68 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { FaUserCircle } from "react-icons/fa";
+import { FaUser, FaBuilding ,FaPlusCircle, FaPlus, FaFolder, FaStickyNote, FaTimes,FaHome  } from "react-icons/fa";
 import "./NavBar.css";
 
 const NavBar = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  // const [isOpen, setIsOpen] = useState(false);
   const [userMenuOpen, setUserMenuOpen] = useState(false);
 
   return (
     <>
-      <nav className={`navbar ${isOpen ? "open" : ""}`}>
-        <div className="navbar-container">
-          {/* الحاوية التي تجمع زر القائمة مع الشعار */}
-          <div className="navbar-header">
-            <button className="menu-toggle" onClick={() => setIsOpen(!isOpen)}>
-              ☰
-            </button>
-            <Link to="/" className="navbar-logo">
+      <nav className="navbar">
+      <div className="NavBarIcons"  
+        style={{display :"flex", justifyContent:"space-around", width:"100%"}}>
+          <Link to="/" className="navbar-logo">
               <h1>عقارات</h1>
-            </Link>
-          </div>
+        </Link>
+        <button className="nav-btn">
+          <Link to="/" className="nav-icon-link"><FaHome/></Link>
+        </button>
 
-          {/* قائمة التنقل */}
-          <ul className={`navbar-menu ${isOpen ? "show" : ""}`}>
-            <li>
-              <Link to="/" className="navbar-link" onClick={() => setIsOpen(false)}>الرئيسية</Link>
-            </li>
-            <li>
-              <Link to="/realEstate" className="navbar-link" onClick={() => setIsOpen(false)}>العقارات</Link>
-            </li>
-            <li>
-              <Link to="/AddProperty" className="navbar-link" onClick={() => setIsOpen(false)}>إضافة عقار</Link>
-            </li>
-            <li>
-              <Link to="/contact" className="navbar-link" onClick={() => setIsOpen(false)}>اتصل بنا</Link>
-            </li>
-          </ul>
+        <button className="nav-btn">
+          <Link to="/realEstate" className="nav-icon-link"><FaBuilding  /></Link>
+        </button>
 
-          {/* أيقونة المستخدم */}
-          <div className="user-menu">
-            <FaUserCircle 
-              size={30} 
-              className="user-icon" 
-              onClick={() => setUserMenuOpen(!userMenuOpen)} 
-            />
-            {userMenuOpen && (
-              <div className="user-dropdown">
-                <Link to="/login" className="user-dropdown-item">تسجيل الدخول</Link>
-                <Link to="/register" className="user-dropdown-item">إنشاء حساب</Link>
-                <Link to="/favorites" className="user-dropdown-item">المفضلات</Link>
-              </div>
-            )}
-          </div>
+        <button className="nav-btn">
+          <Link to="/AddProperty" className="nav-icon-link"><FaPlusCircle /></Link>
+        </button>
+
+        <button className="nav-btn">
+          <FaUser onClick={() => setUserMenuOpen(!userMenuOpen)} />
+        </button>
         </div>
-      </nav>
+{/* 
+        <div className={`fab ${isOpen ? "open" : ""}`} onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? <FaTimes /> : <FaPlus />}
+        </div>
 
-      {/* لتعديل المسافة بين الشريط والمحتوى عند فتح القائمة */}
-      <div className={`content-offset ${isOpen ? "expanded" : ""}`}></div>
+        {isOpen && (
+          <div className="menu">
+            <button className="menu-btn">
+              <Link to="/" className="nav-icon-link">الرئيسية</Link>
+            </button>
+            <button className="menu-btn">
+              <Link to="/realEstate" className="nav-icon-link">العقارات</Link>
+            </button>
+            <button className="menu-btn">
+              <Link to="/AddProperty" className="nav-icon-link">اضافة عقار</Link>
+            </button>
+            <button className="menu-btn">
+              <Link to="/AddProperty" className="nav-icon-link"> 
+                <FaUser onClick={() => setUserMenuOpen(!userMenuOpen)} />
+              </Link>
+            </button>
+          </div>
+        )} */}
+
+        {userMenuOpen && (
+          <div className="user-dropdown">
+            <Link to="/login" className="user-dropdown-item">تسجيل الدخول</Link>
+            <Link to="/register" className="user-dropdown-item">إنشاء حساب</Link>
+            <Link to="/favorites" className="user-dropdown-item">المفضلات</Link>
+          </div>
+        )}
+      </nav>
     </>
   );
 };
