64b21b08b895239011df07c0dbebe0620947ad80
obada63 <ahmadobadaal7jje@gmail.com>
2025-03-22 03:43:20 +0300

    Edit Navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,59 +1,49 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { FaUser, FaBuilding ,FaPlusCircle, FaPlus, FaFolder, FaStickyNote, FaTimes,FaHome  } from "react-icons/fa";
+import { FaUser, FaBuilding, FaPlusCircle, FaPlus, FaHome } from "react-icons/fa";
 import "./NavBar.css";
 
 const NavBar = () => {
-  // const [isOpen, setIsOpen] = useState(false);
   const [userMenuOpen, setUserMenuOpen] = useState(false);
 
   return (
     <>
       <nav className="navbar">
-      <div className="NavBarIcons"  
-        style={{display :"flex", justifyContent:"space-around", width:"100%"}}>
+        <div className="NavBarIcons" style={{ display: "flex", justifyContent: "space-around", width: "100%" }}>
           <Link to="/" className="navbar-logo">
-              <h1>عقارات</h1>
-        </Link>
-        <button className="nav-btn">
-          <Link to="/" className="nav-icon-link"><FaHome/></Link>
-        </button>
+            <h1>عقارات</h1>
+          </Link>
+          
+          {/* Home Button */}
+          <button className="nav-btn">
+            <Link to="/" className="nav-icon-link">
+              <FaHome className="nav-icon" />
+              <span className="nav-text">الرئيسية</span>
+            </Link>
+          </button>
 
-        <button className="nav-btn">
-          <Link to="/realEstate" className="nav-icon-link"><FaBuilding  /></Link>
-        </button>
+          {/* Real Estate Button */}
+          <button className="nav-btn">
+            <Link to="/realEstate" className="nav-icon-link">
+              <FaBuilding className="nav-icon" />
+              <span className="nav-text">عقارات</span>
+            </Link>
+          </button>
 
-        <button className="nav-btn">
-          <Link to="/AddProperty" className="nav-icon-link"><FaPlusCircle /></Link>
-        </button>
+          {/* Add Property Button */}
+          <button className="nav-btn">
+            <Link to="/AddProperty" className="nav-icon-link">
+              <FaPlusCircle className="nav-icon" />
+              <span className="nav-text"> اضافة عقار</span>
+            </Link>
+          </button>
 
-        <button className="nav-btn">
-          <FaUser onClick={() => setUserMenuOpen(!userMenuOpen)} />
-        </button>
+          {/* User Button */}
+          <button className="nav-btn" onClick={() => setUserMenuOpen(!userMenuOpen)}>
+            <FaUser className="nav-icon" />
+            <span className="nav-text">تسجيل</span>
+          </button>
         </div>
-{/* 
-        <div className={`fab ${isOpen ? "open" : ""}`} onClick={() => setIsOpen(!isOpen)}>
-          {isOpen ? <FaTimes /> : <FaPlus />}
-        </div>
-
-        {isOpen && (
-          <div className="menu">
-            <button className="menu-btn">
-              <Link to="/" className="nav-icon-link">الرئيسية</Link>
-            </button>
-            <button className="menu-btn">
-              <Link to="/realEstate" className="nav-icon-link">العقارات</Link>
-            </button>
-            <button className="menu-btn">
-              <Link to="/AddProperty" className="nav-icon-link">اضافة عقار</Link>
-            </button>
-            <button className="menu-btn">
-              <Link to="/AddProperty" className="nav-icon-link"> 
-                <FaUser onClick={() => setUserMenuOpen(!userMenuOpen)} />
-              </Link>
-            </button>
-          </div>
-        )} */}
 
         {userMenuOpen && (
           <div className="user-dropdown">
